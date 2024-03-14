a245d2eceeec49a61bfa492d4298eb3d7c244b10
jackhr <jc2o@mac.com>
2024-03-14 02:21:31 -0400

    Add user authentication and conditional rendering of Index component

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,21 +1,25 @@
 import './App.css'
+import { useContext } from 'react';
 import AuthPage from '../AuthPage/AuthPage'
 import ErrorPage from '../ErrorPage/ErrorPage';
 import LandingPage from '../LandingPage/LandingPage';
 import RootLayout from '../../layouts/Root/RootLayout';
+import { UserContext } from '../../contexts/userContext';
 import {
     Route,
     RouterProvider,
     createBrowserRouter,
     createRoutesFromElements
 } from 'react-router-dom'
+import Index from '../Index/Index';
 
 function App() {
+    const { user } = useContext(UserContext);
 
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={user ? <Index /> : <LandingPage />} />
                 <Route path="/sign-in" element={<AuthPage showLogin={true} />} />
                 <Route path="/sign-up" element={<AuthPage showLogin={false} />} />
             </Route>
