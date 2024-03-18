227412aa48a1ac04682262be24c0e837863e96b5
jackhr <jc2o@mac.com>
2024-03-18 00:49:32 -0400

    Add CSS file and filter options to EditPage

--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -1,3 +1,4 @@
+import "./EditPage.css";
 import swal from "sweetalert";
 import { Photo } from "../../types";
 import { ChevronLeft } from "lucide-react";
@@ -10,6 +11,7 @@ export default function EditPage() {
     const navigate = useNavigate();
     const { photos } = useContext(PhotosContext);
     const [photo, setPhoto] = useState<Photo | undefined>(undefined);
+    const filterNames = ['contrast','grayscale','sepia','vignette','vintage'];
     const goBackToPhotos = useCallback(() => navigate('/', {
         replace: true,
         state: { photosTab: true }
@@ -36,20 +38,23 @@ export default function EditPage() {
         });
     }
 
-    const boxClass = "w-12 h-12 rounded bg-blue-500 text-white flex items-center justify-center cursor-pointer hover:bg-blue-600 active:bg-blue-700";
-
     return (
         <div className="flex flex-col items-center justify-center p-8 h-full relative">
             <ChevronLeft className="absolute top-3 left-3 cursor-pointer bg-gray-300 rounded p-1 h-8 w-8" strokeWidth={3} onClick={goBackToPhotos} />
             <h1 className="mb-6">{photo.name}</h1>
             <img className="h-full" src={photo.sourceURL} alt={photo.name} />
             <div className="flex mt-12 gap-12">
-                {[1, 2, 3, 4, 5].map(i => (
+                {filterNames.map(filterName => (
                     <div
-                        key={i}
-                        className={boxClass}
-                        onClick={() => handleFilterClick(`filter-${i}`)}
-                    >{i}</div>
+                        key={`img-filter-${filterName}`}
+                        className={`flex flex-col items-center gap-2 cursor-pointer relative filter-${filterName}`}
+                        onClick={() => handleFilterClick(`filter ${filterName}`)}
+                    >
+                        <div className="w-12 h-12 rounded flex items-center justify-center cursor-pointer transition-all duration-300 ease-in-out hover:shadow-lg hover:scale-105 hover:bg-gray-100 active:bg-white active:shadow-none relative">
+                            <img className="w-full h-full object-cover" src={photo.sourceURL} alt="" />
+                        </div>
+                        <span>{filterName}</span>
+                    </div>
                 ))}
             </div>
         </div>
